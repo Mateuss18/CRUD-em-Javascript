16868b48f54bb3df73452c8e90ba305930858af3
Mateuss18 <mateusdfl18@gmail.com>
2021-12-07 19:14:35 -0300

    Create concluido

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+const openModal = () => document.getElementById('modal').classList.add('active')
 
+const closeModal = () =>
+  document.getElementById('modal').classList.remove('active')
+
+const tempClient = {
+  nome: 'Mateus',
+  email: 'mateusdfl18@gmail.com',
+  cpf: '4820768813',
+  telefone: '1996489694',
+  endereco: 'Avenida Dr Paulo, 231 SP'
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('dbClient')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("dbClient", JSON.stringify(dbClient))
+//----CRUD----
+
+
+//CREATE
+const createClient = (client) => {
+  const dbClient = getLocalStorage()
+  dbClient.push (client)
+  setLocalStorage(dbClient)
+}
+
+//-----Eventos-----
+document.getElementById('cadastrarCliente').addEventListener('click', openModal)
+
+document.getElementById('modalClose').addEventListener('click', closeModal)
